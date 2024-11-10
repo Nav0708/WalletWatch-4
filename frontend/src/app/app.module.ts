1267f8147bca18c77722fc86beca8b122a27e474
Anusha Panta <55121695+anushapanta@users.noreply.github.com>
2024-11-10 11:44:23 -0800

    module and routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,43 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app.routes';
+import { AppComponent } from './app.component';
+import { BudgetComponent } from './budget/budget.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RemindersComponent } from './reminders/reminders.component';
 
+import { TodoproxyService } from './todoproxy.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    BudgetComponent,
+    ExpensesComponent,
+    HomepageComponent,
+    RemindersComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    BrowserModule,
+    AppRoutingModule,
+    MatTableModule,
+    MatSortModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule
+  ],
+  providers: [provideHttpClient(), TodoproxyService, provideAnimationsAsync()],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { BudgetComponent } from './budget/budget.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RemindersComponent } from './reminders/reminders.component';
+
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'expenses', component: ExpensesComponent },
+//   { path: 'expenses/:id', component: ExpensesComponent },
+  { path: 'budget', component: BudgetComponent },
+  { path: 'budget/:id', component: BudgetComponent },
+  { path: 'reminders', component: RemindersComponent },
+  { path: 'reminders/:id', component: RemindersComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
