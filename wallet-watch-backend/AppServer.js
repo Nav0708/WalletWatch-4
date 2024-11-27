4014c4b2482439158f43d0cfd6b2e262be5e367c
Anusha Panta <55121695+anushapanta@users.noreply.github.com>
2024-11-26 18:52:53 -0800

    expenses add

--- wallet-watch-backend/AppServer.js
+++ wallet-watch-backend/AppServer.js
@@ -15,13 +15,23 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -33,13 +43,13 @@ const express_1 = __importDefault(require("express"));
 // Load environment variables from .env file
 dotenv.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || '';
 const dbUser = process.env.DB_USER || '';
 const dbPassword = process.env.DB_PASSWORD || '';
-const dbHost = process.env.DB_HOST || 'localhost';
-const dbPort = process.env.DB_PORT || '27017';
-const dbName = process.env.DB_NAME || 'walletwatch';
-const dbProtocol = process.env.DB_PROTOCOL || 'mongodb';
+const dbHost = process.env.DB_HOST || '';
+const dbPort = process.env.DB_PORT || '';
+const dbName = process.env.DB_NAME || '';
+const dbProtocol = process.env.DB_PROTOCOL || '';
 // Construct the MongoDB connection string based on authentication presence
 let mongoDBConnection = `${dbProtocol}://`;
 if (dbUser && dbPassword) {
--- wallet-watch-frontend/src/app/expenses/expenses.component.ts
+++ wallet-watch-frontend/src/app/expenses/expenses.component.ts
@@ -7,6 +7,9 @@ import { FormsModule } from '@angular/forms';
 import { ICategoryModel } from '../interfaces/ICategory';
 import { v4 as uuidv4 } from 'uuid'; // Import UUID library for unique ID generation
 import { DatePipe } from '@angular/common';
+
+
+
  
 @Component({
   selector: 'app-expenses',
@@ -48,28 +51,10 @@ export class ExpensesComponent implements OnInit {
       console.error('Error fetching expenses:', error);
     });
     }
-
-
-  // fetchExpenseById(expenseId: string): void {
-  //   this.expenseService.getExpenseById(expenseId).subscribe(
-  //     (data: IExpenseModel) => {
-  //       console.log('individual Expense fetched successfully:', data);
-  //       const formattedDate = this.datePipe.transform(data.date, 'yyyy-MM-dd');
-  //       if (formattedDate) {
-  //         this.currentExpense = { ...data, date: formattedDate };
-  //       }
-  //       console.log('expense : '+ this.currentExpense.expenseId);
-  //       this.router.navigate(['/expenses', this.currentExpense.expenseId]);
-  //     },
-  //     (error) => {
-  //       console.error('Error fetching expense:', error);
-  //     }
-  //   );
-  // }
  
   // Add or update an expense
   onSubmit(): void {
-
+      console.log('onSubmit called'); // Debugging
       this.currentExpense.expenseId = uuidv4();
       this.expenseService.addExpense(this.currentExpense).subscribe(
         () => {
@@ -82,12 +67,7 @@ export class ExpensesComponent implements OnInit {
         }
       );
   }
- 
-  // // Edit an expense
-  // editExpense(expense: IExpenseModel): void {
-  //   this.currentExpense = { ...expense };
-  //   this.editing = true;
-  // }
+
  
   //Edit an expense
   editExpense(expense: IExpenseModel): void {
