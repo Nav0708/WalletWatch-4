02fce25a158431d259289bb586a4be78f6e00b58
Padmaja <pbuggaveeti2811@gmail.com>
2024-12-09 13:05:11 -0800

    modified environment file

--- wallet-watch-backend/azure/environment.ts
+++ wallet-watch-backend/azure/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    hostUrl: "https://skill-sphere.azurewebsites.net"
+    hostUrl: "https://walletwatch-gacbfpg5b4csc4g5.scm.westus-01.azurewebsites.net:443/WalletWatch.git"
  };
