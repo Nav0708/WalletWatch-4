184e83fe9a4750744dde79cb08e4f94893937ede
Navami Bhat <127547240+Nav0708@users.noreply.github.com>
2024-12-11 16:03:50 -0800

    changed the azure link for azure configuration

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    hostUrl: "https://walletwatch-gacbfpg5b4csc4g5.westus-01.azurewebsites.net/",
+    hostUrl: "https://walletwatch-4-g7dxefauf6fwh8hx.westus-01.azurewebsites.net/",
     ///production: false,
     //baseUrl: 'http://localhost:8080/user', // Local development URL
     //expenseUrl: 'http://localhost:8080/walletwatch/expenses', // Local development URL
